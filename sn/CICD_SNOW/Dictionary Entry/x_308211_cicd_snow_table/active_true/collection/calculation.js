f0f39862960f9ecfe6745d1780597b5f4ce0f19a
jayakumarss <jayakumar.sengodan@gmail.com>
2020-01-29 12:49:45 -0500

    'master' branch updated from  https://dev64721.service-now.com no-cicd

--- sn/CICD_SNOW/Dictionary Entry/x_308211_cicd_snow_table/active_true/collection/calculation.js
+++ sn/CICD_SNOW/Dictionary Entry/x_308211_cicd_snow_table/active_true/collection/calculation.js
@@ -3,8 +3,8 @@
  * ClassName   : sys_dictionary
  * Created On  : 2020-01-29 17:01:27
  * Created By  : admin
- * Updated On  : 2020-01-29 17:01:27
- * Updated By  : admin
+ * Updated On  : 2020-01-29 17:20:16
+ * Updated By  : devops
  * URL         : /sys_dictionary.do?sys_id=771813832f6a4010862ea55df699b6f2
  */
 (function calculatedFieldValue(current) {
