59e46e02625e977674d9b867974570097775b185
jayakumarss <jayakumar.sengodan@gmail.com>
2020-01-29 12:06:20 -0500

    'master' branch updated from  https://dev96952.service-now.com no-cicd

---
+++ sn/CICD_SNOW/Dictionary Entry/x_308211_cicd_snow_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : CICD_SNOW
+ * ClassName   : sys_dictionary
+ * Created On  : 2020-01-29 17:01:27
+ * Created By  : admin
+ * Updated On  : 2020-01-29 17:01:27
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=771813832f6a4010862ea55df699b6f2
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
